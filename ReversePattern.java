e1502404d693dab90e60398ab1867ebf66d460cd
Rajesh-181 <sbrajesh2005@gmail.com>
2026-06-08 20:20:56 +0530

    Initial commit

--- ReversePattern.java
+++ ReversePattern.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReversePatternSimple {
+public class ReversePattern{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
