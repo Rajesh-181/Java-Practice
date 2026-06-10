3e47a81c72847f95542fe208b4ad69c196dd516c
Rajesh <162716051+Rajesh-181@users.noreply.github.com>
2026-06-10 09:32:01 +0530

    Add files via upload

---
+++ LeftTriangle.java
@@ -0,0 +1,18 @@
+public class LeftTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i ; j++) {
+                if(j==i+j){
+                    System.err.print("#");
+                }
+                else{
+                    System.out.print("* ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ middle_of_list.java
@@ -0,0 +1,13 @@
+public class middle_of_list {
+ 
+    public ListNode middleNode(ListNode head) {
+        if(head==null || head.next == null) return head;
+        ListNode slow=head,fast=head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+
